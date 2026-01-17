19e6cb126eb8e05ebfcd185b1ed4a752071f1512
saintsHr <hiqkrs@gmail.com>
2026-01-17 13:16:16 -0300

    feat(render): Mesh & RenderObject

--- include/granite/render.hpp
+++ include/granite/render.hpp
@@ -1,15 +1,33 @@
 #pragma once
 
 #include "glad/glad.h"
+#include "granite/vector.hpp"
 
-namespace gr{
+namespace gr::Render{
     
-class Render{
-public:
-    Render() = delete;
+void init();
 
-    static void init();
-
+struct Transform{
+    gr::Vec3 position;
+    gr::Vec3 rotation;
+    gr::Vec3 scale;
 };
 
-}
+class Mesh{
+public:
+    Mesh();
+    ~Mesh();
+    void upload(const float* vertices, uint32_t vertexCount);
+    void draw(GLenum drawMode = GL_TRIANGLES) const;
+private:
+    GLuint vbo_, vao_;
+    uint32_t vertexCount_;
+};
+
+class RenderObject{
+public:
+    Mesh* mesh;
+    Transform transform;
+};
+
+};
--- src/render.cpp
+++ src/render.cpp
@@ -2,10 +2,51 @@
 
 #include "GLFW/glfw3.h"
 
-namespace gr{
+namespace gr::Render{
 
-void Render::init(){
+void init(){
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) return;
+}
+
+void Mesh::upload(const float* vertices, uint32_t vertexCount){
+    vertexCount_ = vertexCount;
+
+    glBindVertexArray(vao_);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo_);
+
+    glBufferData(
+        GL_ARRAY_BUFFER,
+        vertexCount * 3 * sizeof(float),
+        vertices,
+        GL_STATIC_DRAW
+    );
+
+    glVertexAttribPointer(
+        0,
+        3,
+        GL_FLOAT,
+        GL_FALSE,
+        3 * sizeof(float),
+        (void*)0
+    );
+
+    glEnableVertexAttribArray(0);
+    glBindVertexArray(0);
+}
+
+void Mesh::draw(GLenum drawMode) const{
+    glBindVertexArray(vao_);
+    glDrawArrays(drawMode, 0, vertexCount_);
+}
+
+Mesh::~Mesh(){
+    glDeleteVertexArrays(1, &vao_);
+    glDeleteBuffers(1, &vbo_);
+}
+
+Mesh::Mesh(){
+    glGenVertexArrays(1, &vao_);
+    glGenBuffers(1, &vbo_);
 }
 
 };
