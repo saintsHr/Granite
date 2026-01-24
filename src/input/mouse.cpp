0245d6e8f35a8d4c229bbb6013a3a9d36c1bb7d1
saintsHr <hiqkrs@gmail.com>
2026-01-24 19:48:42 -0300

    refactor(input): separated keyboard and mouse input

--- src/input/mouse.cpp
+++ src/input/mouse.cpp
@@ -1,16 +1,6 @@
-#include <GLFW/glfw3.h>
-
-#include "granite/input/input.hpp"
+#include "granite/input/mouse.hpp"
 
 namespace gr::Input{
-
-bool KeyPressed(gr::Window window, Input::Key key){
-    if (glfwGetKey(window.getRaw(), static_cast<int>(key)) == GLFW_PRESS){
-        return true;
-    } else {
-        return false;
-    }
-}
 
 void MouseLocked(gr::Window window, bool state){
     if (state)  glfwSetInputMode(window.getRaw(), GLFW_CURSOR, GLFW_CURSOR_DISABLED);
