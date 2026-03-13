1c92e90ac81fc7ee7be70bf7242ce31f85e4a52b
saintsHr <hiqkrs@gmail.com>
2026-03-13 16:44:45 -0300

    chore(scene): fixed CI warnings

--- src/granite/scene/object.cpp
+++ src/granite/scene/object.cpp
@@ -1,23 +1,14 @@
 #include "granite/scene/object.hpp"
 #include "granite/renderer/renderer.hpp"
 
-namespace gr::Scene {
+gr::Vec3 calculateDirection(gr::Vec3 rotation, gr::Direction direction) {
+    float rx = gr::Math::deg2rad(rotation.x);
+    float ry = gr::Math::deg2rad(rotation.y);
+    float rz = gr::Math::deg2rad(rotation.z);
 
-void Object::draw() {
-    part_.material  = material;
-    part_.transform = transform;
-    part_.mesh      = &mesh;
-    gr::Renderer::addToQueue(part_);
-}
-
-gr::Vec3 Object::getDirection(gr::Direction direction) {
-    float rx = gr::Math::deg2rad(transform.rotation.x);
-    float ry = gr::Math::deg2rad(transform.rotation.y);
-    float rz = gr::Math::deg2rad(transform.rotation.z);
-
-    float cx = cos(rx), sx = sin(rx);
-    float cy = cos(ry), sy = sin(ry);
-    float cz = cos(rz), sz = sin(rz);
+    float cx = std::cos(rx), sx = std::sin(rx);
+    float cy = std::cos(ry), sy = std::sin(ry);
+    float cz = std::cos(rz), sz = std::sin(rz);
 
     float Rx[3][3] = {{1,0,0},{0,cx,-sx},{0,sx,cx}};
     float Ry[3][3] = {{cy,0,sy},{0,1,0},{-sy,0,cy}};
@@ -47,6 +38,19 @@ gr::Vec3 Object::getDirection(gr::Direction direction) {
     result.z = R[2][0]*local.x + R[2][1]*local.y + R[2][2]*local.z;
 
     return result;
+}
+
+namespace gr::Scene {
+
+void Object::draw() {
+    part_.material  = material;
+    part_.transform = transform;
+    part_.mesh      = &mesh;
+    gr::Renderer::addToQueue(part_);
+}
+
+gr::Vec3 Object::getDirection(gr::Direction direction) {
+    return calculateDirection(transform.rotation, direction);
 }
 
 void ModelObject::load(gr::Assets::Model& model, std::shared_ptr<gr::Renderer::Shader> shader) {
@@ -72,42 +76,7 @@ void ModelObject::draw() {
 }
 
 gr::Vec3 ModelObject::getDirection(gr::Direction direction) {
-    float rx = gr::Math::deg2rad(transform.rotation.x);
-    float ry = gr::Math::deg2rad(transform.rotation.y);
-    float rz = gr::Math::deg2rad(transform.rotation.z);
-
-    float cx = cos(rx), sx = sin(rx);
-    float cy = cos(ry), sy = sin(ry);
-    float cz = cos(rz), sz = sin(rz);
-
-    float Rx[3][3] = {{1,0,0},{0,cx,-sx},{0,sx,cx}};
-    float Ry[3][3] = {{cy,0,sy},{0,1,0},{-sy,0,cy}};
-    float Rz[3][3] = {{cz,-sz,0},{sz,cz,0},{0,0,1}};
-
-    float R[3][3] = {0};
-    for(int i=0;i<3;i++)
-        for(int j=0;j<3;j++)
-            for(int k=0;k<3;k++)
-                for(int l=0;l<3;l++)
-                    R[i][j] += Rz[i][k] * Ry[k][l] * Rx[l][j];
-
-    gr::Vec3 local;
-    switch(direction){
-        case gr::Direction::FRONT:  local = {0,0,1}; break;
-        case gr::Direction::BACK:   local = {0,0,-1}; break;
-        case gr::Direction::RIGHT:  local = {1,0,0}; break;
-        case gr::Direction::LEFT:   local = {-1,0,0}; break;
-        case gr::Direction::UP:     local = {0,1,0}; break;
-        case gr::Direction::DOWN:   local = {0,-1,0}; break;
-        default: return {0,0,0};
-    }
-
-    gr::Vec3 result;
-    result.x = R[0][0]*local.x + R[0][1]*local.y + R[0][2]*local.z;
-    result.y = R[1][0]*local.x + R[1][1]*local.y + R[1][2]*local.z;
-    result.z = R[2][0]*local.x + R[2][1]*local.y + R[2][2]*local.z;
-
-    return result;
+    return calculateDirection(transform.rotation, direction);
 }
 
 }
